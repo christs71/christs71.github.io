bf3d91e1f7fe44d222af3e58a8d231079b3e591f
christs71 <62073073+christs71@users.noreply.github.com>
2020-04-26 15:33:02 -0500

    version6

---
+++ wdc6.js
@@ -0,0 +1,68 @@
+(function () {
+    var myConnector = tableau.makeConnector();
+
+    myConnector.getSchema = function (schemaCallback) {
+    var cols = [
+        { id : "groupGuid", alias : "groupGuid", dataType : tableau.dataTypeEnum.string},
+       	 {  id : "groupName", alias : "groupName", dataType : tableau.dataTypeEnum.string },
+     	 
+    ];
+
+    var tableInfo = {
+        id : "bitlyGroups",
+        alias : "Bitly Groups",
+        columns : cols
+    };
+
+    schemaCallback([tableInfo]);
+};
+
+        //Gets bitlinks for a group
+      var settings = {
+        "url": "https://api-ssl.bitly.com/v4/groups",
+        "method": "GET",
+        "timeout": 0,
+        "headers": {
+          "Authorization": "Bearer 63ef42580f0dfc8517612b31d2aa1c9c37cba526"
+        },
+      };
+
+    myConnector.getData = function (table, doneCallback) {
+    		var groupData = tableau.connectionData;
+            $.ajax(settings), function (response) {
+    	            	
+           tableau.log(resp);
+           
+        	var feat = resp.data.groups,
+           tableData = [];
+           
+
+        // Iterate over the JSON object
+        for (var i = 0; i < feat.length; i++) {
+            tableData.push({
+                "groupGuid": feat[i].guid,
+                "groupName": feat[i].name,
+                "count": feat[i].likes.count,
+                "tags": feat[i].tags
+
+            });
+        }
+
+        table.appendRows(tableData);
+        doneCallback();
+    });
+
+    };
+
+    tableau.registerConnector(myConnector);
+
+    $(document).ready(function () {
+
+    $("#submitButton").click(function () {
+    	var tickerSymbol = $('#ticker').val().trim();
+    	tableau.connectionData = groupData;
+        tableau.connectionName = "Results for " + groupData;
+        tableau.submit();
+    });
+});
+})();
