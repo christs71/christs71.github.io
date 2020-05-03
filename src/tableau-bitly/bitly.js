584546b5617802e1ec2575142b084bb8fdde2b88
Michael Ecklund <info@michaelbrentecklund.com>
2020-05-02 22:04:04 -0500

    updated

---
+++ src/tableau-bitly/bitly.js
@@ -0,0 +1,396 @@
+var bitly = window[ 'bitly' ] || {};
+
+bitly = (
+
+	/**
+	 * Essentially a "class".
+	 */
+	function( $ ) {
+
+		/**
+		 *
+		 * @type {{data: {groups: []}, allowed_groups: [string], auth_token: string, fetched: {channels: null, campaigns: null, groups: null, clicks: null, bitlinks: null}}}
+		 */
+		var self = {
+			allowed_groups: [
+				'Analytics',
+			],
+			auth_token: '63ef42580f0dfc8517612b31d2aa1c9c37cba526',
+			fetched: {
+				'groups': null,
+				'bitlinks': null,
+				'clicks': null,
+				'channels': null,
+				'campaigns': null,
+			},
+			data: {
+				'groups': [],
+			},
+		};
+
+		/**
+		 * Essentially a "class" constructor.
+		 */
+		self.initialize = async function() {
+
+			console.log( 'bitly Initialized!' );
+
+			try {
+
+				console.log( self[ 'fetched' ] );
+				console.log( self[ 'data' ] );
+
+				await self.fetch();
+
+				console.log( self[ 'fetched' ] );
+				console.log( self[ 'data' ] );
+
+			} catch ( error ) {
+				console.log( error );
+			}
+
+		};
+
+
+		/**
+		 * Retrieves all the data from the remote source(s).
+		 *
+		 * @returns {Promise<boolean>}
+		 */
+		self.fetch = async function() {
+
+			try {
+
+				var groups = await self.get_groups();
+
+				if ( groups ) {
+
+					if ( groups[ 'length' ] >= 1 ) {
+
+						for ( var i = 0; i < groups[ 'length' ]; i ++ ) {
+
+							var group = groups[ i ];
+
+							if ( self.allowed_groups.indexOf( group[ 'name' ] ) != - 1 ) {
+								self[ 'data' ][ 'groups' ].push( group );
+							}
+
+						}
+
+					}
+
+					groups = self[ 'data' ][ 'groups' ];
+
+					if ( groups[ 'length' ] >= 1 ) {
+
+						for ( var i = 0; i < groups[ 'length' ]; i ++ ) {
+
+							var group = groups[ i ];
+
+							// Get Campaigns
+							self[ 'data' ][ 'groups' ][ i ][ 'campaigns' ] = await self.get_campaigns( group );
+
+							var campaigns = self[ 'data' ][ 'groups' ][ i ][ 'campaigns' ];
+
+							if ( campaigns[ 'length' ] >= 1 ) {
+
+								for ( var x = 0; x < campaigns[ 'length' ]; x ++ ) {
+
+									var campaign = campaigns[ x ];
+
+									// Get Campaign Channels
+									self[ 'data' ][ 'groups' ][ i ][ 'campaigns' ][ x ][ 'channels' ] = await self.get_channels( group, campaign );
+
+								}
+
+							}
+
+							// Get Bitlinks
+							self[ 'data' ][ 'groups' ][ i ][ 'bitlinks' ] = await self.get_bitlinks( group );
+
+							var bitlinks = self[ 'data' ][ 'groups' ][ i ][ 'bitlinks' ][ 'links' ];
+
+							if ( bitlinks[ 'length' ] >= 1 ) {
+
+								for ( var y = 0; y < bitlinks[ 'length' ]; y ++ ) {
+
+									var bitlink = bitlinks[ y ];
+
+									// Get Bitlink Clicks
+									self[ 'data' ][ 'groups' ][ i ][ 'bitlinks' ][ 'links' ][ y ][ 'clicks' ] = await self.get_clicks( bitlink );
+
+								}
+
+							}
+
+						}
+
+					}
+
+				}
+
+				return true;
+
+			} catch ( error ) {
+				console.log( error );
+			}
+
+		};
+
+
+		/**
+		 * Retrieves all GHroups from Bitly.
+		 *
+		 * @returns {Promise<unknown>}
+		 */
+		self.get_groups = function() {
+
+			return new Promise( async function( resolve, reject ) {
+
+				var groups = [];
+
+				$.ajax( {
+					'url': 'https://api-ssl.bitly.com/v4/groups',
+					'method': 'GET',
+					'timeout': 0,
+					'headers': {
+						'Authorization': 'Bearer ' + self[ 'auth_token' ],
+					},
+					'success': function( response ) {
+
+						console.log( 'success', response );
+
+						if ( response[ 'groups' ] !== undefined && Array.isArray( response[ 'groups' ] ) ) {
+							groups = response[ 'groups' ];
+						}
+
+						self[ 'fetched' ][ 'groups' ] = true;
+
+						resolve( groups );
+
+					},
+					'error': function( error ) {
+
+						self[ 'fetched' ][ 'groups' ] = false;
+
+						console.log( 'error', error );
+
+						reject( error );
+
+					},
+				} );
+
+			} );
+
+		};
+
+
+		/**
+		 * Retrieves all Bitlinks in Group from Bitly.
+		 *
+		 * @param group
+		 * @returns {Promise<unknown>}
+		 */
+		self.get_bitlinks = function( group ) {
+
+			return new Promise( async function( resolve, reject ) {
+
+				var bitlinks = [];
+
+				$.ajax( {
+					'url': 'https://api-ssl.bitly.com/v4/groups/' + group[ 'guid' ] + '/bitlinks',
+					'method': 'GET',
+					'timeout': 0,
+					'headers': {
+						'Authorization': 'Bearer ' + self[ 'auth_token' ],
+					},
+					'success': function( response ) {
+
+						console.log( 'success', response );
+
+						bitlinks = response;
+						self[ 'fetched' ][ 'bitlinks' ] = true;
+
+						resolve( bitlinks );
+
+					},
+					'error': function( error ) {
+
+						self[ 'fetched' ][ 'bitlinks' ] = false;
+
+						console.log( 'error', error );
+
+						reject( error );
+
+					},
+				} );
+
+			} );
+
+		};
+
+
+		/**
+		 * Retrieves all Clicks on a Bitlink from Bitly.
+		 *
+		 * @param bitlink
+		 * @returns {Promise<unknown>}
+		 */
+		self.get_clicks = function( bitlink ) {
+
+			return new Promise( async function( resolve, reject ) {
+
+				await setTimeout( async function() {
+
+					var clicks = [];
+
+					$.ajax( {
+						'url': 'https://api-ssl.bitly.com/v4/bitlinks/' + bitlink[ 'id' ] + '/clicks',
+						'method': 'GET',
+						'timeout': 0,
+						'headers': {
+							'Authorization': 'Bearer ' + self[ 'auth_token' ],
+						},
+						'success': function( response ) {
+
+							console.log( 'success', response );
+
+							if ( response[ 'link_clicks' ] !== undefined && Array.isArray( response[ 'link_clicks' ] ) ) {
+								clicks = response[ 'link_clicks' ];
+							}
+
+							self[ 'fetched' ][ 'clicks' ] = true;
+
+							resolve( clicks );
+
+						},
+						'error': function( error ) {
+
+							self[ 'fetched' ][ 'clicks' ] = false;
+
+							console.log( 'error', error );
+
+							reject( error );
+
+						},
+					} );
+
+				}, 500 );
+
+			} );
+
+		};
+
+
+		/**
+		 * Retrieves all Campaigns in Group from Bitly.
+		 *
+		 * @param group
+		 * @returns {Promise<unknown>}
+		 */
+		self.get_campaigns = function( group ) {
+
+			return new Promise( async function( resolve, reject ) {
+
+				var campaigns = [];
+
+				$.ajax( {
+					'url': 'https://api-ssl.bitly.com/v4/campaigns',
+					'method': 'GET',
+					'timeout': 0,
+					'headers': {
+						'Authorization': 'Bearer ' + self[ 'auth_token' ],
+					},
+					'data': {
+						'group_guid': group[ 'guid' ],
+					},
+					'success': function( response ) {
+
+						console.log( 'success', response );
+
+						if ( response[ 'campaigns' ] !== undefined && Array.isArray( response[ 'campaigns' ] ) ) {
+							campaigns = response[ 'campaigns' ];
+						}
+
+						self[ 'fetched' ][ 'campaigns' ] = true;
+
+						resolve( campaigns );
+
+					},
+					'error': function( error ) {
+
+						self[ 'fetched' ][ 'campaigns' ] = false;
+
+						console.log( 'error', error );
+
+						reject( error );
+
+					},
+				} );
+
+			} );
+
+		};
+
+
+		/**
+		 * Retrieves all Campaign Channels in Group from Bitly.
+		 *
+		 * @param group
+		 * @param campaign
+		 * @returns {Promise<unknown>}
+		 */
+		self.get_channels = function( group, campaign ) {
+
+			return new Promise( async function( resolve, reject ) {
+
+				await setTimeout( async function() {
+
+					var channels = [];
+
+					$.ajax( {
+						'url': 'https://api-ssl.bitly.com/v4/channels',
+						'method': 'GET',
+						'timeout': 0,
+						'headers': {
+							'Authorization': 'Bearer ' + self[ 'auth_token' ],
+						},
+						'data': {
+							'group_guid': group[ 'guid' ],
+							'campaign_guid': campaign[ 'guid' ],
+						},
+						'success': function( response ) {
+
+							console.log( 'success', response );
+
+							if ( response[ 'channels' ] !== undefined && Array.isArray( response[ 'channels' ] ) ) {
+								channels = response[ 'channels' ];
+							}
+
+							self[ 'fetched' ][ 'channels' ] = true;
+
+							resolve( channels );
+
+						},
+						'error': function( error ) {
+
+							self[ 'fetched' ][ 'channels' ] = false;
+
+							console.log( 'error', error );
+
+							reject( error );
+
+						},
+					} );
+
+				}, 500 );
+
+			} );
+
+		};
+
+		return self;
+
+	}
+
+)( jQuery );
---
+++ src/tableau-bitly/tableau_wdc.js
@@ -0,0 +1,651 @@
+var tableau_wdc = window[ 'tableau_wdc' ] || {};
+
+tableau_wdc = (
+
+	function( $ ) {
+
+		/**
+		 *
+		 * @type {{tables: {link_clicks: {}, deeplinks: {}, channels: {}, campaigns: {}, references: {}, custom_bitlinks: {}, groups: {},
+		 *     links: {}, tags: {}}, columns: {link_clicks: [], deeplinks: [], channels: [], campaigns: [], references: [],
+		 *     custom_bitlinks: [], groups: [], links: [], tags: []}, rows: {link_clicks: [], deeplinks: [], channels: [], campaigns: [],
+		 *     references: [], custom_bitlinks: [], groups: [], links: [], tags: []}}}
+		 */
+		var self = {
+			tables: {
+				'groups': {},
+				'links': {},
+				'link_clicks': {},
+				'custom_bitlinks': {},
+				'deeplinks': {},
+				'tags': {},
+				'channels': {},
+				'campaigns': {},
+				'references': {},
+			},
+			columns: {
+				'groups': [],
+				'links': [],
+				'link_clicks': [],
+				'custom_bitlinks': [],
+				'deeplinks': [],
+				'tags': [],
+				'channels': [],
+				'campaigns': [],
+				'references': [],
+			},
+			rows: {
+				'groups': [],
+				'links': [],
+				'link_clicks': [],
+				'custom_bitlinks': [],
+				'deeplinks': [],
+				'tags': [],
+				'channels': [],
+				'campaigns': [],
+				'references': [],
+			},
+		};
+
+		/**
+		 * Essentially a "class" constructor.
+		 */
+		self.initialize = function() {
+
+			console.log( 'tableau_wdc Initialized!' );
+			self.do_actions();
+			self.initialize_tableau();
+
+		};
+
+
+		/**
+		 * Handles events.
+		 */
+		self.do_actions = function() {
+
+			$( document ).ready( function() {
+
+				$( '#submitButton' ).on( 'click', function() {
+
+					tableau.connectionName = 'Bitly Data'; // This will be the data source name in Tableau
+					tableau.submit(); // This sends the connector object to Tableau
+
+				} );
+
+			} );
+
+		};
+
+
+		/**
+		 * Sets table columns and defines data types, etc.
+		 *
+		 * @returns {boolean}
+		 */
+		self.set_columns = function() {
+
+
+			self[ 'columns' ][ 'groups' ] = [
+				{
+					id: 'guid',
+					alias: 'id',
+					dataType: tableau[ 'dataTypeEnum' ][ 'string' ],
+				},
+				{
+					id: 'name',
+					dataType: tableau[ 'dataTypeEnum' ][ 'string' ],
+				},
+				{
+					id: 'is_active',
+					alias: 'status',
+					dataType: tableau[ 'dataTypeEnum' ][ 'bool' ],
+				},
+			];
+
+
+			self[ 'columns' ][ 'links' ] = [
+				{
+					id: 'created_at',
+					alias: 'date_created',
+					dataType: tableau[ 'dataTypeEnum' ][ 'datetime' ],
+				},
+				{
+					id: 'id',
+					dataType: tableau[ 'dataTypeEnum' ][ 'string' ],
+				},
+				{
+					id: 'link',
+					dataType: tableau[ 'dataTypeEnum' ][ 'string' ],
+				},
+				{
+					id: 'long_url',
+					dataType: tableau[ 'dataTypeEnum' ][ 'string' ],
+				},
+				{
+					id: 'title',
+					dataType: tableau[ 'dataTypeEnum' ][ 'string' ],
+				},
+				{
+					id: 'archived',
+					alias: 'status',
+					dataType: tableau[ 'dataTypeEnum' ][ 'bool' ],
+				},
+				{
+					id: 'unit_reference',
+					dataType: tableau[ 'dataTypeEnum' ][ 'string' ],
+				},
+			];
+
+
+			self[ 'columns' ][ 'link_clicks' ] = [
+				{
+					id: 'link_id',
+					dataType: tableau[ 'dataTypeEnum' ][ 'string' ],
+				},
+				{
+					id: 'date',
+					dataType: tableau[ 'dataTypeEnum' ][ 'datetime' ],
+				},
+				{
+					id: 'clicks',
+					alias: 'count',
+					dataType: tableau[ 'dataTypeEnum' ][ 'int' ],
+				},
+			];
+
+
+			self[ 'columns' ][ 'custom_bitlinks' ] = [
+				{
+					id: 'link_id',
+					dataType: tableau[ 'dataTypeEnum' ][ 'string' ],
+				},
+				{
+					id: 'value',
+					dataType: tableau[ 'dataTypeEnum' ][ 'string' ],
+				},
+			];
+
+
+			self[ 'columns' ][ 'deeplinks' ] = [
+				{
+					id: 'link_id',
+					dataType: tableau[ 'dataTypeEnum' ][ 'string' ],
+				},
+				{
+					id: 'value',
+					dataType: tableau[ 'dataTypeEnum' ][ 'string' ],
+				},
+			];
+
+
+			self[ 'columns' ][ 'tags' ] = [
+				{
+					id: 'link_id',
+					dataType: tableau[ 'dataTypeEnum' ][ 'string' ],
+				},
+				{
+					id: 'name',
+					dataType: tableau[ 'dataTypeEnum' ][ 'string' ],
+				},
+			];
+
+
+			self[ 'columns' ][ 'campaigns' ] = [
+				{
+					id: 'guid',
+					alias: 'id',
+					dataType: tableau[ 'dataTypeEnum' ][ 'string' ],
+				},
+				{
+					id: 'group_guid',
+					alias: 'group_id',
+					dataType: tableau[ 'dataTypeEnum' ][ 'string' ],
+				},
+				{
+					id: 'name',
+					dataType: tableau[ 'dataTypeEnum' ][ 'string' ],
+				},
+				{
+					id: 'description',
+					dataType: tableau[ 'dataTypeEnum' ][ 'string' ],
+				},
+				{
+					id: 'created_at',
+					alias: 'date_created',
+					dataType: tableau[ 'dataTypeEnum' ][ 'datetime' ],
+				},
+			];
+
+
+			self[ 'columns' ][ 'channels' ] = [
+				{
+					id: 'guid',
+					alias: 'id',
+					dataType: tableau[ 'dataTypeEnum' ][ 'string' ],
+				},
+				{
+					id: 'group_guid',
+					alias: 'group_id',
+					dataType: tableau[ 'dataTypeEnum' ][ 'string' ],
+				},
+				{
+					id: 'name',
+					dataType: tableau[ 'dataTypeEnum' ][ 'string' ],
+				},
+			];
+
+
+			self[ 'columns' ][ 'references' ] = [
+				{
+					id: 'table_name',
+					dataType: tableau[ 'dataTypeEnum' ][ 'string' ],
+				},
+				{
+					id: 'table_id',
+					dataType: tableau[ 'dataTypeEnum' ][ 'string' ],
+				},
+				{
+					id: 'key',
+					dataType: tableau[ 'dataTypeEnum' ][ 'string' ],
+				},
+				{
+					id: 'value',
+					dataType: tableau[ 'dataTypeEnum' ][ 'string' ],
+				},
+			];
+
+
+			return true;
+
+		};
+
+
+		/**
+		 * Sets table data.
+		 */
+		self.set_tables = function() {
+
+			self[ 'tables' ][ 'groups' ] = {
+				id: 'groups',
+				columns: self[ 'columns' ][ 'groups' ],
+			};
+
+			self[ 'tables' ][ 'links' ] = {
+				id: 'links',
+				columns: self[ 'columns' ][ 'links' ],
+			};
+
+			self[ 'tables' ][ 'link_clicks' ] = {
+				id: 'link_clicks',
+				columns: self[ 'columns' ][ 'link_clicks' ],
+			};
+
+			self[ 'tables' ][ 'custom_bitlinks' ] = {
+				id: 'custom_bitlinks',
+				columns: self[ 'columns' ][ 'custom_bitlinks' ],
+			};
+
+			self[ 'tables' ][ 'deeplinks' ] = {
+				id: 'deeplinks',
+				columns: self[ 'columns' ][ 'deeplinks' ],
+			};
+
+			self[ 'tables' ][ 'references' ] = {
+				id: 'references',
+				columns: self[ 'columns' ][ 'references' ],
+			};
+
+			self[ 'tables' ][ 'tags' ] = {
+				id: 'tags',
+				columns: self[ 'columns' ][ 'tags' ],
+			};
+
+			self[ 'tables' ][ 'channels' ] = {
+				id: 'channels',
+				columns: self[ 'columns' ][ 'channels' ],
+			};
+
+			self[ 'tables' ][ 'campaigns' ] = {
+				id: 'campaigns',
+				columns: self[ 'columns' ][ 'campaigns' ],
+			};
+
+		};
+
+
+		/**
+		 * Download the data.
+		 *
+		 * @returns {Promise<boolean>}
+		 */
+		self.fetch_data = async function() {
+
+			try {
+
+				var data = [];
+
+				if ( bitly !== undefined ) {
+
+					await bitly.initialize(); // Basically "instantiates" the "class".
+					data = bitly[ 'data' ][ 'groups' ];
+
+				}
+
+				self.set_rows( data );
+
+				return true;
+
+			} catch ( error ) {
+				console.log( error );
+			}
+
+		};
+
+
+		/**
+		 * Sets all of the data for the tables.
+		 *
+		 * @param groups
+		 * @returns {boolean}
+		 */
+		self.set_rows = function( groups ) {
+
+			if ( groups === undefined || Array.isArray( groups ) === false ) {
+				return false;
+			}
+
+			if ( groups[ 'length' ] <= 0 ) {
+				return false;
+			}
+
+			for ( var g = 0; g < groups[ 'length' ]; g ++ ) {
+
+				var group = groups[ g ];
+
+				// do something with a Group
+
+				self[ 'rows' ][ 'groups' ].push( {
+					'guid': group[ 'guid' ],
+					'name': group[ 'name' ],
+					'is_active': group[ 'is_active' ],
+				} );
+
+				var campaigns = group[ 'campaigns' ];
+
+				if ( campaigns[ 'length' ] >= 1 ) {
+
+					for ( var ca = 0; ca < campaigns[ 'length' ]; ca ++ ) {
+
+						var campaign = campaigns[ ca ];
+
+						// do something with a Campaign
+
+						self[ 'rows' ][ 'campaigns' ].push( {
+							'created': campaign[ 'created' ],
+							'guid': campaign[ 'guid' ],
+							'group_guid': campaign[ 'group_guid' ],
+							'name': campaign[ 'name' ],
+							'description': campaign[ 'description' ],
+						} );
+
+						var campaign_references = campaign[ 'references' ];
+						var campaign_reference_length = Object.keys( campaign_references )[ 'length' ];
+
+						if ( campaign_reference_length > 0 ) {
+
+							for ( var campaign_reference_key in campaign_references ) {
+
+								var campaign_reference_value = campaign_references[ campaign_reference_key ];
+
+								// do something with a Campaign Reference
+
+								self[ 'rows' ][ 'references' ].push( {
+									'table_name': 'campaigns',
+									'table_id': campaign[ 'guid' ],
+									'key': campaign_reference_key,
+									'value': campaign_reference_value,
+								} );
+
+							}
+
+						}
+
+						var channels = campaign[ 'channels' ];
+
+						if ( channels[ 'length' ] >= 1 ) {
+
+							for ( var ch = 0; ch < channels[ 'length' ]; ch ++ ) {
+
+								var channel = channels[ ch ];
+
+								// do something with a Channel
+
+								self[ 'rows' ][ 'channels' ].push( {
+									'guid': channel[ 'guid' ],
+									'group_guid': channel[ 'group_guid' ],
+									'name': channel[ 'name' ],
+								} );
+
+								var channel_references = channel[ 'references' ];
+								var channel_reference_length = Object.keys( channel_references )[ 'length' ];
+
+								if ( channel_reference_length > 0 ) {
+
+									for ( var channel_reference_key in channel_references ) {
+
+										var channel_reference_value = channel_references[ channel_reference_key ];
+
+										// do something with a Channel Reference
+
+										self[ 'rows' ][ 'references' ].push( {
+											'table_name': 'channels',
+											'table_id': channel[ 'guid' ],
+											'key': channel_reference_key,
+											'value': channel_reference_value,
+										} );
+
+									}
+
+								}
+
+							}
+
+
+						}
+
+					}
+
+				}
+
+				var links = group[ 'bitlinks' ][ 'links' ];
+
+				if ( links[ 'length' ] >= 1 ) {
+
+					for ( var l = 0; l < links[ 'length' ]; l ++ ) {
+
+						var link = links[ l ];
+
+						// do something with a Link
+
+						var link_data = {
+							'created_at': link[ 'created_at' ],
+							'id': link[ 'id' ],
+							'link': link[ 'link' ],
+							'long_url': link[ 'long_url' ],
+							'title': link[ 'title' ],
+							'archived': link[ 'archived' ],
+						};
+
+						var clicks = link[ 'clicks' ];
+
+						if ( clicks[ 'length' ] >= 1 ) {
+
+							for ( var lc = 0; lc < clicks[ 'length' ]; lc ++ ) {
+
+								var click = clicks[ lc ];
+
+								// do something with a Link Click
+
+								link_data[ 'unit_reference' ] = click[ 'unit_reference' ];
+
+								self[ 'rows' ][ 'link_clicks' ].push( {
+									'link_id': link[ 'id' ],
+									'date': click[ 'date' ],
+									'clicks': click[ 'clicks' ], // <-- holy tongue twisters
+								} );
+
+							}
+
+						}
+
+						// Pushed to link rows AFTER Link clicks, because Link Clicks append Unit Reference to the Link.
+						self[ 'rows' ][ 'links' ].push( link_data );
+
+						var custom_bitlinks = link[ 'custom_bitlinks' ];
+
+						if ( custom_bitlinks[ 'length' ] >= 1 ) {
+
+							for ( var cb = 0; cb < custom_bitlinks[ 'length' ]; cb ++ ) {
+
+								var custom_bitlink = custom_bitlinks[ cb ];
+
+								// do something with a Custom Bitlink
+
+								self[ 'rows' ][ 'custom_bitlinks' ].push( {
+									'link_id': link[ 'id' ],
+									'value': custom_bitlink,
+								} );
+
+							}
+
+						}
+
+						var deeplinks = link[ 'deeplinks' ];
+
+						if ( deeplinks[ 'length' ] >= 1 ) {
+
+							for ( var dl = 0; dl < deeplinks[ 'length' ]; dl ++ ) {
+
+								var deeplink = deeplinks[ dl ];
+
+								// do something with a Deeplink
+
+								self[ 'rows' ][ 'deeplinks' ].push( {
+									'link_id': link[ 'id' ],
+									'value': deeplink,
+								} );
+
+							}
+
+						}
+
+						var tags = link[ 'tags' ];
+
+						if ( tags[ 'length' ] >= 1 ) {
+
+							for ( var t = 0; t < tags[ 'length' ]; t ++ ) {
+
+								var tag = tags[ t ];
+
+								// do something with a Link Tag
+
+								self[ 'rows' ][ 'tags' ].push( {
+									'link_id': link[ 'id' ],
+									'name': tag,
+								} );
+
+							}
+
+						}
+
+						var link_references = link[ 'references' ];
+						var link_reference_length = Object.keys( link_references )[ 'length' ];
+
+						if ( link_reference_length > 0 ) {
+
+							for ( var link_reference_key in link_references ) {
+
+								var link_reference_value = link_references[ link_reference_key ];
+
+								// do something with a Link Reference
+
+								self[ 'rows' ][ 'references' ].push( {
+									'table_name': 'links',
+									'table_id': link[ 'guid' ],
+									'key': link_reference_key,
+									'value': link_reference_value,
+								} );
+
+							}
+
+						}
+
+					}
+
+				}
+
+			}
+
+		};
+
+
+		self.initialize_tableau = function() {
+
+			console.log( 'tableau initialized!' );
+
+			/**
+			 * Create the connector object
+			 */
+			self[ 'connector' ] = tableau.makeConnector();
+
+			/**
+			 * Define the schema
+			 */
+			self[ 'connector' ].getSchema = function( schemaCallback ) {
+
+				self.set_columns();
+				self.set_tables();
+
+				schemaCallback( Object.values( self[ 'tables' ] ) );
+
+			};
+
+			/**
+			 * Download/Set the data to be used in Tableau.
+			 */
+			self[ 'connector' ].getData = async function( table, doneCallback ) {
+
+				try {
+
+					await self.fetch_data();
+
+					var data = [];
+
+					if ( self[ 'rows' ][ table[ 'tableInfo' ][ 'id' ] ] !== undefined ) {
+						data = self[ 'rows' ][ table[ 'tableInfo' ][ 'id' ] ];
+					}
+
+					table.appendRows( data );
+
+					doneCallback();
+
+				} catch ( error ) {
+					console.log( error );
+				}
+
+			};
+
+			tableau.registerConnector( self[ 'connector' ] );
+
+		};
+
+		return self;
+
+	}
+
+)( jQuery );
+
+if ( tableau !== undefined && bitly !== undefined ) {
+	tableau_wdc.initialize(); // Basically "instantiates" the "class".
+}
